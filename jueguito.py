e18db531a5e4dfd4bfa6ead69f3d849c23606f17
Laura Valentina Gonzalez <102635399+valentinalgn@users.noreply.github.com>
2022-05-25 20:24:21 -0500

    Update jueguito.py

--- jueguito.py
+++ jueguito.py
@@ -1,3 +1,9 @@
-Hola(Laura)
-Hola(Val)
-:)
+from email import message
+import tkinter
+from tkinter import messagebox
+
+ventana=tkinter.Tk()
+ventana.geometry("360x300")
+etiqueta=tkinter.Label(ventana, text=("GUAYABITA"))
+
+etiqueta.pack(fill=tkinter.X)
