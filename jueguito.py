6831e47fc2282fe08f9dfc9488f191e6cf2fd57b
Robert Arturo Jaime Nieto <102542899+RobertJaime@users.noreply.github.com>
2022-05-28 23:16:54 -0500

    Update jueguito.py

--- jueguito.py
+++ jueguito.py
@@ -1,13 +1,19 @@
 from email import message
+from re import A
 import tkinter
 from tkinter import messagebox
+from typing import Text
+import random
 
 ventana=tkinter.Tk()
 ventana.geometry("360x300")
-etiqueta=tkinter.Label(ventana, text=("GUAYABITA"))
+etiqueta=tkinter.Label(ventana, text=("GUAYABITA"),bg="blue")
 
 etiqueta.pack(fill=tkinter.X)
 
+
+
+# botones jugadores input nombre plata
 def jugador1():
     def cyg():
         x=cajatextonombre.get()
@@ -23,33 +29,33 @@ def jugador1():
     
     j1=tkinter.Toplevel()
     j1.geometry("380x300")
-    j1.config(background="chocolate")
+    j1.config(background="plum2")
     tj1=tkinter.Label(j1,text="hola jugador 1")
     tj1.pack(padx=4,pady=5,ipadx=6,ipady=5,fill=tkinter.X)
     nombre=tkinter.Label(j1,text="Nombre:")
     nombre.place(relx=0.1,rely=0.4)
     cajatextonombre=tkinter.Entry(j1)
     cajatextonombre.place(relx=0.3,rely=0.4)
-    boton=tkinter.Button(j1,text="ok",command=cyg)
+    boton=tkinter.Button(j1,text="aceptar",command=cyg)
     boton.pack(side=tkinter.BOTTOM)
     tj1p=tkinter.Label(j1,text="Ingrese su dinero total")
     tj1p.place(relx=0.1,rely=0.5)
     cajatextoplata=tkinter.Entry(j1)
     cajatextoplata.place(relx=0.5,rely=0.5)
-    
-    [20:28] ROBERT ARTURO JAIME NIETO
-    def jugador2():
-def cyg():
+
+def jugador2():
+    def cyg():
         x=cajatextonombre.get()
         y=cajatextoplata.get()
         jugadores["b"]["nombre"]=x
         jugadores["b"]["plata"]=y
         pl=int(y)
         print(jugadores)
-if (pl >=200):
+        if (pl >=200):
             j2.withdraw()
-else:
+        else:
             messagebox.showerror("Error","Su dinero total debe ser mayor a $200")
+    
     j2=tkinter.Toplevel()
     j2.geometry("380x300")
     j2.config(background="peru")
@@ -65,19 +71,20 @@ else:
     tj2p.place(relx=0.1,rely=0.5)
     cajatextoplata=tkinter.Entry(j2)
     cajatextoplata.place(relx=0.5,rely=0.5)
-​[20:28] ROBERT ARTURO JAIME NIETO
-    def jugador3():
-def cyg():
+
+def jugador3():
+    def cyg():
         x=cajatextonombre.get()
         y=cajatextoplata.get()
         pl=int(y)
         jugadores["c"]["nombre"]=x
         jugadores["c"]["plata"]=y
         print(jugadores)
-if (pl >=200):
-            j3.withdraw()
-else:
+        if (pl >=200):
+            j3.destroy()
+        else:
             messagebox.showerror("Error","Su dinero total debe ser mayor a $200")
+        
 
     j3=tkinter.Toplevel()
     j3.geometry("380x300")
@@ -95,6 +102,17 @@ else:
     cajatextoplata=tkinter.Entry(j3)
     cajatextoplata.place(relx=0.5,rely=0.5)
 
+
+
+#botones inicio pestañas jugadores info
+boton1=tkinter.Button(ventana,command=jugador1,text="jugador 1")
+boton2=tkinter.Button(ventana,command=jugador2,text="jugador 2")
+boton3=tkinter.Button(ventana,command=jugador3,text="jugador 3")
+
+
+boton1.place(relx=0.1,rely=0.5)
+boton2.place(relx=0.45,rely=0.5)
+boton3.place(relx=0.8,rely=0.5)
 #botones inicio y reglas
 def reglas():
     
@@ -119,93 +137,208 @@ bregla.place(relx=0.6,rely=0.7)
 #Boton juego 
 
 def inicio():
-    ventana.withdraw()
-    ini=tkinter.Toplevel()
-    ini.geometry("380x300")
-    ini.config(background="light cyan")
-    tini=tkinter.Label(ini,text="aqui va el juego",bg="light cyan")
-    tini.pack(padx=4,pady=5,ipadx=6,ipady=5,fill=tkinter.X)
-    
 
-bini=tkinter.Button(ventana,command=inicio,text="Jugar",bg="green",fg="white")
-bini.place(relx=0.3,rely=0.7)
-#botones apuestas
+
+    
+    pl1=int(jugadores["a"]["plata"])
+    pl2=int(jugadores["b"]["plata"])
+    pl3=int(jugadores["c"]["plata"])
+
+        
+
+
+    if (pl1!=0 and pl2!=0 and pl3!=0):
+        ventana.withdraw()
+        ini=tkinter.Toplevel()
+        ini.geometry("700x450")
+        ini.config(background="light cyan")
+        tini=tkinter.Label(ini,text="aqui va el juego",bg="light cyan")
+        tini.pack(padx=4,pady=5,ipadx=6,ipady=5,fill=tkinter.X)
+        #info jugadores
+        turno_jugador1=tkinter.Label(ini,text="TURNO DEL JUGADOR 1 😎",font=("Helvetica",10))
+        turno_jugador1.place(relx=0.4,rely=0.2)       
+
+        a=jugadores["a"]["nombre"]
+        b=jugadores["b"]["nombre"]
+        c=jugadores["c"]["nombre"]
+
+        
+
+        p1=jugadores["a"]["plata"]
+        p2=jugadores["b"]["plata"]
+        p3=jugadores["c"]["plata"]
+
+        pini1=int(p1)-200
+        pini2=int(p2)-200
+        pini3=int(p3)-200
+
+        jugadores["a"]["plata"]=pini1
+        jugadores["b"]["plata"]=pini2
+        jugadores["c"]["plata"]=pini3
+        
+        print(pini1,pini2,pini3)
+        
+        #nombres y dinero en bolsillo
+        nombre1=tkinter.Label(ini,text=a)
+        nombre2=tkinter.Label(ini,text=b)
+        nombre3=tkinter.Label(ini,text=c)
+
+        plata1=tkinter.Label(ini,text=pini1)
+        plata2=tkinter.Label(ini,text=pini2)
+        plata3=tkinter.Label(ini,text=pini3)
+
+        nombre1.place(relx=0.1,rely=0.8)
+        nombre2.place(relx=0.45,rely=0.8)
+        nombre3.place(relx=0.8,rely=0.8)
+
+        plata1.place(relx=0.1,rely=0.84)
+        plata2.place(relx=0.45,rely=0.84)
+        plata3.place(relx=0.8,rely=0.84)
+        #boton resta
+        def resta():
+            a1=int(jugadores["a"]["plata"])
+            a2=a1-1
+            jugadores["a"]["plata"]=a2
+            plata1["text"]=a2
+            print(jugadores)
+        restar=tkinter.Button(ini,command=resta,text="restar")
+        restar.place(relx=0.8,rely=0.5)
+    #valentina
+    #descuento dinero cuando saque 1 o 6 
+        #boton dado
+        
         def dado():
+            pm=int(jugadores["mesa"]["plata"])
+
+            pj1=jugadores["a"]["plata"]
+            pj2=jugadores["b"]["plata"]
+            pj3=jugadores["c"]["plata"]
+
+            
+            if pm<=0:
+                messagebox.showinfo("fin del juego","no queda plata en la mesa el ganador es..")
+                ini.destroy
+            if (pj1<=0 or pj2<=0 or pj3<=0):
+                messagebox.showinfo("fin del juego","el juego a terminado")
+                ini.destroy
 
             turno=jugadores["dado"]["turno"]
             turint=int(turno)
 
-
+            
+            
+            
 
             d=random.randint(1,6)
             ap1=jugadores["a"]["apuesta"]
             ap2=jugadores["b"]["apuesta"]
             ap3=jugadores["c"]["apuesta"]
-            
-            
-            if ((turno==0 or turno==1 or turno==5) and (d==1 or d==6) ) :
 
-                    messagebox.showinfo("pierdes","pones 200")                    
+            pm=int(jugadores["mesa"]["plata"])
+            platamesa["text"]=f"Dinero en la mesa: ${pm}"
+            plata1["text"]=jugadores["a"]["plata"]
+            plata2["text"]=jugadores["b"]["plata"]
+            plata3["text"]=jugadores["c"]["plata"]
 
-            elif((turno==3) and (d==1 or d==6)):
+            if ((turno==0 or turno==1 or turno==6) and (d==1 or d==6) ) :                    
+                if turno==6:
+                    mg=pm+200
+                    jp_1=pj1-200
+                    jugadores["a"]["plata"]=jp_1
+                    messagebox.showinfo("pierdes","jugador 1 pones 200")  
 
-                messagebox.showinfo("pierdes","pones 200")            
+                    jugadores["mesa"]["plata"]=mg
 
+                elif turno==1:
+                    mg=pm+200
+                    jp_2=pj2-200
+                    jugadores["b"]["plata"]=jp_2
+                    messagebox.showinfo("pierdes","jugador 2 pones 200")  
+                    jugadores["mesa"]["plata"]=mg
+                    
+
+                elif((turno==3) and (d==1 or d==5)):
+                    messagebox.showinfo("pierdes","jugador 3 pones 200")
+                    mg=pm+200
+                    jp_3=pj3-200
+                    jugadores["c"]["plata"]=jp_3
+
+                    jugadores["mesa"]["plata"]=mg
+                            
             else:
-
                 if turno==0  or turno==5:
-
                     turno_jugador1=tkinter.Label(ini,text="TURNO DEL JUGADOR 1 😎",font=("Helvetica",10))
-
                     turno_jugador1.place(relx=0.4,rely=0.2)
-
-                    if d>jugadores["dado"]["numero"]:
-
+                    
+                    
+                    if d>jugadores["dado"]["numero"] and turno==0:
                         messagebox.showinfo("GANASTE",f"Ganaste toma{ap1}")
+                        mg=pm-ap1
+                        jugadores["mesa"]["plata"]=mg
+                        j1p=pj1+ap1
+                        jugadores["a"]["plata"]=j1p
 
-                    else:
 
+                    if d<=jugadores["dado"]["numero"] and turno==1:
                         messagebox.showinfo("perdiste",f"Perdiste, pon en la mesa :{ap1}")
-
-
+                        mg=pm+ap1
+                        jugadores["mesa"]["plata"]=mg
+                        j1p=pj1-ap1
+                        jugadores["a"]["plata"]=j1p
 
                 elif turno==1 or turno==2:
-
                     turno_jugador2=tkinter.Label(ini,text="TURNO DEL JUGADOR 2 😎",font=("Helvetica",10))
-
                     turno_jugador2.place(relx=0.4,rely=0.2)
-
-                    if d>jugadores["dado"]["numero"]:
-
+                                    
+                    if d>jugadores["dado"]["numero"] and turno==2:
                         messagebox.showinfo("GANASTE",f"Ganaste toma{ap2}")
+                        mg=pm-ap2
+                        j2p=pj2+ap2
+                        jugadores["b"]["plata"]=j2p
 
-                    else:
-
+                        jugadores["mesa"]["plata"]=mg
+                    if d<=jugadores["dado"]["numero"] and turno==2:
                         messagebox.showinfo("perdiste",f"Perdiste, pon en la mesa :{ap2}")
+                        mg=pm+ap2
+                        j2p=pj2-ap2
+                        jugadores["b"]["plata"]=j2p
 
-elif turno==3 or turno==4:
+                        jugadores["mesa"]["plata"]=mg
 
+                elif turno==3 or turno==4:
                     turno_jugador3=tkinter.Label(ini,text="TURNO DEL JUGADOR 3 😎",font=("Helvetica",10))
-
                     turno_jugador3.place(relx=0.4,rely=0.2)
-
-                    if d>jugadores["dado"]["numero"]:
-
+                                     
+                    if d>jugadores["dado"]["numero"] and turno==4:
                         messagebox.showinfo("GANASTE",f"Ganaste toma{ap3}")
-
-                    else:
-
+                        mg=pm-ap3
+                        j3p=pj3+ap3
+                        jugadores["c"]["plata"]=j3p
+                        jugadores["mesa"]["plata"]=mg
+                    if d<=jugadores["dado"]["numero"] and turno==4:
                         messagebox.showinfo("perdiste",f"Perdiste, pon en la mesa :{ap3}")
+                        mg=pm+ap3
+                        j3p=pj3-ap3
+                        jugadores["c"]["plata"]=j3p
+                        jugadores["mesa"]["plata"]=mg
 
+                
+                
+
+            jugadores["dado"]["numero"]=d
             
             
-   
+            #turnos 
             if turint<=5:
                 trn=turint+1
                 jugadores["dado"]["turno"]=trn
                 
             else:
-                jugadores["dado"]["turno"]=trn
+                jugadores["dado"]["turno"]=1
+            
+            
+
+            
             
             #mensaje apuesta
             #turno jugador 1
@@ -356,90 +489,56 @@ elif turno==3 or turno==4:
             print(jugadores)
             
             
+            
                    
         
+        apuesta=tkinter.Button(ini,text="Tirar dados",command=dado)
+        apuesta.place(relx=0.1,rely=0.5)
+        #plata en la mesa
+        pm=int(jugadores["mesa"]["plata"])
+        platamesa=tkinter.Label(ini,text=f"Dinero en la mesa: ${pm}")
+        platamesa.place(relx=0.45,rely=0.6)
         
+
+
+                
+    else:
+         messagebox.showerror("Error","llene la informacion de los 3 jugadores")
+
+bini=tkinter.Button(ventana,command=inicio,text="Jugar",bg="green",fg="white")
+bini.place(relx=0.3,rely=0.7)
+
+#codigo juego
 
 jugadores={
     "a":{
         "nombre":"",
-        "plata":"",
+        "plata":"0",
         "apuesta":"",
     },
     "b":{
         "nombre":"",
-        "plata":"",
+        "plata":"0",
         "apuesta":"",
     },
     "c":{
         "nombre":"",
-        "plata":"",
+        "plata":"0",
         "apuesta":"",
     },
+    "mesa":{
+        "plata":"600"
+    },
+    "dado":{
+        "numero":"",
+        "turno":"0" ,       
+    },
 }
+#Codigo dado por turnos de cada jugador 
 
-#Dado
-import random
-#Apuestas
 
-j1_dinero=int(input("Ingrese el valor inicial con el que jugara:"))
-j2_dinero=int(input("Ingrese el valor inicial con el que jugara:"))
-j3_dinero=int(input("Ingrese el valor inicial con el que jugara:"))
 
-if j1_dinero and j3_dinero and j2_dinero<1000:
-    print("No es posible jugar con un valor inicial menor a $1.000")
-else:
-    print("Listos para jugar")
-#En mesa
-mesa=int
-jp1=j1_dinero-200
-jp2=j2_dinero-200
-jp3=j3_dinero-200
-mesadinero=(200+200+200)
-print ("En la mesa hay:", mesadinero)
-print("Turno jugador 1...")
+    
 
-#Turno 1 
-apuesta_inicial=(int)
-queda1=(int)
-d=random.randint(1,6)
-d1=random.randint(1,6)
-print("Tirando el dado...")
-print(f"Numero:", d)
-if (d==1 or d==6):
-    print("Perdiste,Debe poner $200")
-if (d==2 or 3 or 4 or 5):
-    apuesta_inicial=int(input("Ingrese su apuesta:"))
-    d1=random.randint(1,6)
-    print("Tirando dado de nuevo...")
-    print("Numero:",d1)
-if (d1>d):
-    print("GANASTE, Toma el dinero_apostado")
-else:print("PERDISTE, Pon en la mesa el dinero_apostado")
-mesa=int
-mesa1=int
-mesa2=int
-mesa=mesadinero+200
-mesa1=mesadinero+apuesta_inicial
-mesa2=mesadinero-apuesta_inicial
-if (mesa)<=0:
-    print("El juego termino")
-if j1_dinero or j2_dinero or j3_dinero==(j1_dinero+j2_dinero+j3_dinero):
-    print("El juego termino")
-if j1_dinero or j2_dinero or j3_dinero<=0:
-    print("El juego termino")
-
-if (d==1 or d==6):
-    queda1=jp1-200
-    print("Total:",queda1)
-    print("Ahora en la mesa hay:", mesa)
-if (d1>d):
-    queda1=(jp1)+apuesta_inicial
-    print("Tu total es de",queda1)
-    print("Ahora en la mesa hay:",mesa2 )
-else:
-    queda1=(jp1)-apuesta_inicial
-    print("Tu total es de:",queda1)
-    print("Ahora en la mesa hay:",mesa1)
-
-print("Es el turno del jugador 2")
+print(jugadores)
+ventana.mainloop()
